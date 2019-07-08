7c6c979710c77b2a5cc2ce3d5858733a94805981
wqy <1442963504@qq.com>
2019-07-08 15:32:34 +0800

    init

---
+++ docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+if [ $CERT ];then
+	if ! [[ $(acme.sh --list) =~ $CERT ]]; then
+		acme.sh --issue --fullchain-file /cert/cert.crt --key-file /cert/key.crt --standalone -d $CERT
+	fi
+fi
+trap "echo stop && killall crond && exit 0" SIGTERM SIGINT 
+crond && while true; do sleep 1; done;
