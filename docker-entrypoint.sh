8c10936825fd8575a813e5b73e1dcd881cffe346
wqy <1442963504@qq.com>
2019-07-08 15:48:13 +0800

    修复

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 if [ $CERT ];then
 	if ! [[ $(acme.sh --list) =~ $CERT ]]; then
-		acme.sh --issue --fullchain-file /cert/cert.crt --key-file /cert/key.crt --standalone -d $CERT
+		acme.sh --issue --fullchain-file /cert/cert.crt --key-file /cert/cert.key --standalone -d $CERT
 	fi
 fi
 trap "echo stop && killall crond && exit 0" SIGTERM SIGINT 
