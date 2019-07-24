5a23dae280ffcc8b756ef852c958ac3585395d8e
wqy <1442963504@qq.com>
2019-07-24 13:23:39 +0800

    增加证书前缀

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
+CRT="/cert/"${CERT_PREFIX}"cert.crt"
+KEY="/cert/"${CERT_PREFIX}"cert.key"
 if [ $CERT ];then
 	if ! [[ $(acme.sh --list) =~ $CERT ]]; then
-		acme.sh --issue --fullchain-file /cert/cert.crt --key-file /cert/cert.key --standalone -d $CERT
+		acme.sh --issue --fullchain-file $CRT --key-file $KEY --standalone -d $CERT
 	fi
 fi
 trap "echo stop && killall crond && exit 0" SIGTERM SIGINT 
