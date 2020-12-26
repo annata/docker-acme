a7cd7fc0057bedbe47e08174ecebbf4c45dd1e27
wqy <jjj9000000@vip.qq.com>
2020-12-26 12:13:19 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ CRT="/cert/"${CERT_PREFIX}"cert.crt"
 KEY="/cert/"${CERT_PREFIX}"cert.key"
 if [ $CERT ];then
 	if ! [[ $(acme.sh --list) =~ $CERT ]]; then
-		acme.sh --issue --fullchain-file $CRT --key-file $KEY --standalone -d $CERT
+		acme.sh --issue --fullchain-file $CRT --key-file $KEY --standalone -d $CERT || true
 	fi
 fi
 trap "echo stop && killall crond && exit 0" SIGTERM SIGINT 
